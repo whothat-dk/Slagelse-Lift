9ea3e0451303a57426a1d4298f9c30ea6c1727e2
Stefan Korfitz <stefan@korfitz.com>
2017-07-27 18:15:28 +0200

    ...

---
+++ wp-content/themes/whothat/404.php
@@ -0,0 +1,11 @@
+<!-- NAVIGATION + CONTENT -->
+<?php get_header(); ?>
+    <div class="content">
+        <p>404 page</p>
+    </div><!-- .content -->
+
+<!-- SIDEBAR -->
+<?php get_sidebar(); ?>
+
+<!-- FOOTER -->
+<?php get_footer(); ?>
