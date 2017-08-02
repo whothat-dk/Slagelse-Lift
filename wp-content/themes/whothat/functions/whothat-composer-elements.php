00d765972a85cb2d57478b9887732065e941f26c
Stefan Korfitz <stefan@korfitz.com>
2017-08-02 16:50:23 +0200

    ...

--- wp-content/themes/whothat/functions/whothat-composer-elements.php
+++ wp-content/themes/whothat/functions/whothat-composer-elements.php
@@ -17,7 +17,7 @@ add_action( 'vc_before_init', 'vc_before_init_actions' );
 function vc_before_init_actions() {
 
 	// Require new custom Element
-	//require_once( get_template_directory().'/composer/imageBox.php' );
+	require_once( get_template_directory().'/composer/imageBox.php' );
 	//require_once( get_template_directory().'/composer/infoBox.php' );
 	//require_once( get_template_directory().'/composer/employeeBox.php' );
 	//require_once( get_template_directory().'/composer/faIconBox.php' );
