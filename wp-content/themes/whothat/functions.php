49fab396b6cc3fc0c689614e5c26a581bd7e42e7
Stefan Korfitz <stefan@korfitz.com>
2017-08-03 20:36:00 +0200

    css på footeren tilføjet

--- wp-content/themes/whothat/functions.php
+++ wp-content/themes/whothat/functions.php
@@ -7,13 +7,14 @@
  */
 defined( 'ABSPATH' ) or die();
 define( 'WHOTHAT_SITE_VERSION', '1.0.0' );
+
 /**
- * include
- * other functions
+ * include : CUSTOM FUNCTIONS
  */
 //include_once get_template_directory() . '/functions/lana-menu.php';
 include_once get_template_directory() . '/functions/whothat-theme.php';
 include_once get_template_directory() . '/functions/whothat-theme-customizer.php';
+include_once get_template_directory() . '/functions/whothat-composer-elements.php';
 
 /**
  * REMOVE WP STUFF
@@ -25,20 +26,24 @@ remove_action('wp_head', 'wlwmanifest_link');
 remove_action('wp_head', 'wp_generator');
 remove_action('wp_head', 'rest_output_link_wp_head', 10 );
 remove_action('wp_head', 'wp_oembed_add_discovery_links', 10 );
+
+/**
+ * ACQUIRE FONT-AWESOME
+ */
 function enqueue_our_required_stylesheets(){
 	wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
 }
 add_action('wp_enqueue_scripts','enqueue_our_required_stylesheets');
 
 /**
- * CSS Control
+ * CSS
  */
 wp_enqueue_style( 'fonts', get_template_directory_uri() .'/css/fonts/fonts.css' );
 wp_enqueue_style( 'responsive', get_template_directory_uri() .'/css/responsive.css' );
 wp_enqueue_style( 'animate', get_template_directory_uri() .'/css/animate.css' );
 
 /**
- * Javascript
+ * JAVASCRIPT
  */
 //wp_enqueue_script( 'main', get_template_directory_uri() . '/js/scripts.js', array ( 'jquery' ), 1.1, true);
 wp_register_script( 'hamburger', get_template_directory_uri() . '/js/hamburger.js', array( 'jquery' ), '1.0.0' );
@@ -61,10 +66,3 @@ function main_nav() {
 	);
 }
 add_action( 'init', 'main_nav' );
-
-
-/*
-require_once( get_template_directory().'/woocommerce/woocommerce-settings.php' );
-*/
-
-include_once get_template_directory() . '/functions/whothat-composer-elements.php';
