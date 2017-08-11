e4d0be24e239bd2adcca7584c522f6d5689070a3
Stefan Korfitz <stefan@korfitz.com>
2017-08-11 18:49:05 +0200

    tæt på at være færdig med footeren...

--- wp-content/themes/whothat/footer.php
+++ wp-content/themes/whothat/footer.php
@@ -1,42 +1,95 @@
-	<footer class="row">
-        <!-- Grid container -->
-        <div class="gc">
-            <!-- UPPER PART -->
-            <div class="col-12 footer_top footer_facebook">
-                <div class="col-6">
-                    <div class="vc_fa_iconbox_wrap ">
-                        <div class="vc_fa_iconbox_container">
-                            <a href="https://www.facebook.com/slagelselift/" target="_blank" class="vc_fa_iconbox_link">
-                                <i class="fa fa-facebook vc_fa_icon" aria-hidden="true"></i>
-                                FØLG MED PÅ FACEBOOK
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-6">
-                    <div class="vc_fa_iconbox_wrap ">
-                        <div class="vc_fa_iconbox_container">
-                            <a href="https://www.facebook.com/slagelselift/" target="_blank" class="vc_fa_iconbox_link">
-                                <i class="fa fa-facebook vc_fa_icon" aria-hidden="true"></i>
-                                FØLG MED PÅ FACEBOOK
-                            </a>
-                        </div>
+
+<footer class="row">
+    <!-- Grid container -->
+    <div class="gc">
+        <!-- UPPER PART -->
+        <div class="col-12 footer_top footer_facebook">
+            <div class="col-6">
+                <div class="vc_fa_iconbox_wrap ">
+                    <div class="vc_fa_iconbox_container">
+                        <a href="https://www.facebook.com/slagelselift/" target="_blank" class="vc_fa_iconbox_link">
+                            <i class="fa fa-facebook vc_fa_icon" aria-hidden="true"></i>
+                            FØLG MED PÅ FACEBOOK
+                        </a>
                     </div>
                 </div>
             </div>
-            <!-- LOWER PART -->
-            <div class="col-12 footer_bottom">
-                <!-- : RIGHT -->
-                <div class="col-6">
-
-                </div>
-                <!-- : RIGHT -->
-                <div class="col-6">
-
+            <div class="col-6">
+                <div class="vc_fa_iconbox_wrap ">
+                    <div class="vc_fa_iconbox_container">
+                        <a href="https://www.facebook.com/slagelselift/" target="_blank" class="vc_fa_iconbox_link">
+                            <i class="fa fa-facebook vc_fa_icon" aria-hidden="true"></i>
+                            FØLG MED PÅ FACEBOOK
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
-	</footer>
-	<? wp_footer(); ?>
-	</body>
+        <!-- MIDDLE PART -->
+        <div class="footer_logo">
+            <a href='<?php echo esc_url( home_url( '/' ) ); ?>' title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>' rel='home'>
+                <img src='<?php echo esc_url( get_theme_mod( 'footer_logo' ) ); ?>' alt='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>'>
+            </a>
+        </div>
+        <!-- LOWER PART -->
+        <div class="col-12 footer_bottom">
+            <!-- : LEFT -->
+            <div class="col-6">
+                <!-- SLAGELSE -->
+                <div class="col-3">
+                    <div class="footer_company_info">
+                        <h5>SLAGELSE AFDELING</h5>
+                        <p><span class="footer_info"><?= get_theme_mod( 'footer_slagelse_vej' ); ?></span></p>
+                        <p><span class="footer_info"><?= get_theme_mod( 'footer_slagelse_by' ); ?></span></p>
+                    </div>
+                </div>
+                <!-- HERLEV -->
+                <div class="col-3">
+                    <div class="footer_company_info">
+                        <h5>HERLEV AFDELING</h5>
+                        <p><span class="footer_info"><?= get_theme_mod( 'footer_herlev_vej' ); ?></span></p>
+                        <p><span class="footer_info"><?= get_theme_mod( 'footer_herlev_by' ); ?></span></p>
+                    </div>
+                </div>
+                <!-- KONTAKT -->
+                <div class="col-3">
+                    <div class="footer_company_info">
+                        <h5>KONTAKT</h5>
+                        <p><a href="mailto:<?= get_theme_mod( 'footer_contact_mail' ); ?>" class="footer_contact_mail"><?= get_theme_mod( 'footer_contact_mail' ); ?></a></p>
+                        <p><a href="tel:<?= get_theme_mod( 'footer_contact_tel' ); ?>" class="footer_contact_tel mt30"><?= get_theme_mod( 'footer_contact_tel' ); ?></a></p>
+                    </div>
+                </div>
+            </div>
+            <!-- : RIGHT -->
+            <div class="col-6">
+                <div class="footer_logo_container">
+                    <!-- IMAGE #1 -->
+                    <div class="footer_logo">
+                        <img src='<?php echo esc_url( get_theme_mod( 'footer_logo_1' ) ); ?>'>
+                    </div>
+                    <!-- IMAGE #2 -->
+                    <div class="footer_logo">
+                        <img src='<?php echo esc_url( get_theme_mod( 'footer_logo_2' ) ); ?>'>
+                    </div>
+                    <!-- IMAGE #3 -->
+                    <div class="footer_logo">
+                        <img src='<?php echo esc_url( get_theme_mod( 'footer_logo_3' ) ); ?>'>
+                    </div>
+                    <!-- IMAGE #4 -->
+                    <div class="footer_logo">
+                        <img src='<?php echo esc_url( get_theme_mod( 'footer_logo_4' ) ); ?>'>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-12">
+            <!-- : DISCLAIMER -->
+            <div class="footer_disclaimer">
+                <span class="footer_disclaimer"><?= get_theme_mod( 'footer_disclaimer' ); ?></span>
+            </div>
+        </div>
+    </div>
+</footer>
+<? wp_footer(); ?>
+</body>
 </html>
--- wp-content/themes/whothat/functions/whothat-theme-customizer.php
+++ wp-content/themes/whothat/functions/whothat-theme-customizer.php
@@ -7,7 +7,7 @@
  */
 defined( 'ABSPATH' ) or die();
 
-//ADD SUPPORT FOR THEME CUSTOMIZATION
+// ADD SUPPORT FOR THEME CUSTOMIZATION
 add_action( 'customize_register', 'theme_options' );
 
 /**
@@ -15,43 +15,43 @@ add_action( 'customize_register', 'theme_options' );
  */
 function theme_options( $wp_customize ) {
 
-	//Header settings
+	// Header settings
 	$wp_customize->add_section( 'header_options',
 		array(
-			'title'       => __( 'Header Settings', 'priisholm' ),
+			'title'       => __( 'Header Settings', 'slagelselift' ),
 			'priority'    => 100,
-			'description' => __('Change header options here.', 'priisholm'),
+			'description' => __('Change header options here.', 'slagelselift'),
 		)
 	);
 
-	//footer settings
+	// Footer settings
 	$wp_customize->add_section( 'footer_options',
 		array(
-			'title'       => __( 'Footer Settings', 'espenheins' ),
+			'title'       => __( 'Footer Settings', 'slagelselift' ),
 			'priority'    => 100,
-			'description' => __('Change footer options here.', 'espenheins'),
+			'description' => __('Change footer options here.', 'slagelselift'),
 		)
 	);
 
-	//Header
+	// Header
 	$wp_customize->add_setting( 'header_logo' );
 	$wp_customize->add_setting( 'resp_logo' );
 	$wp_customize->add_setting( 'header_background' );
 
 	$wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'header_logo', array(
-		'label'    => __( 'Logo', 'slagelse lift' ),
-		'section'  => 'header_options',
-		'settings' => 'header_logo',
+		'label'         => __( 'Logo', 'slagelselift' ),
+		'section'       => 'header_options',
+		'settings'      => 'header_logo',
 	) ) );
 
 	$wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'resp_logo', array(
-		'label'         => __( 'Mobile menu logo', 'slagelse lift' ),
+		'label'         => __( 'Mobile menu logo', 'slagelselift' ),
 		'section'       => 'header_options',
 		'settings'      => 'resp_logo',
 	) ) );
 
 	$wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'header_background', array(
-		'label'         => __( 'Header background image', 'slagelse lift' ),
+		'label'         => __( 'Header background image', 'slagelselift' ),
 		'section'       => 'header_options',
 		'settings'      => 'header_background',
 		'height'        => 200,
@@ -60,75 +60,105 @@ function theme_options( $wp_customize ) {
 		'header-text'   => false,
 	) ) );
 
-	//Footer
+	// Footer
 	$wp_customize->add_setting( 'footer_logo' );
-	$wp_customize->add_setting( 'footer_company' );
-	$wp_customize->add_setting( 'footer_address' );
-	$wp_customize->add_setting( 'footer_city' );
-	$wp_customize->add_setting( 'footer_cvr' );
-	$wp_customize->add_setting( 'footer_tel' );
-	$wp_customize->add_setting( 'footer_email' );
+	$wp_customize->add_setting( 'footer_slagelse_vej' );
+	$wp_customize->add_setting( 'footer_slagelse_by' );
+	$wp_customize->add_setting( 'footer_herlev_vej' );
+	$wp_customize->add_setting( 'footer_herlev_by' );
+	$wp_customize->add_setting( 'footer_contact_mail' );
+	$wp_customize->add_setting( 'footer_contact_tel' );
 	$wp_customize->add_setting( 'footer_logo_1' );
 	$wp_customize->add_setting( 'footer_logo_2' );
+	$wp_customize->add_setting( 'footer_logo_3' );
+	$wp_customize->add_setting( 'footer_logo_4' );
+	$wp_customize->add_setting( 'footer_disclaimer' );
 
+	// Footer : LOGO
 	$wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'footer_logo', array(
-		'label'    => __( 'Logo', 'espenheins' ),
-		'section'  => 'footer_options',
-		'settings' => 'footer_logo',
+		'label'         => __( 'LOGO', 'slagelselift' ),
+		'section'       => 'footer_options',
+		'settings'      => 'footer_logo',
 	) ) );
 
-
-	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'footer_company', array(
-		'label'    => __( 'Company', 'espenheins' ),
-		'section'  => 'footer_options',
-		'settings' => 'footer_company',
-		'type'     => 'text',
+	// Footer : SLAGELSE
+	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'footer_slagelse_vej', array(
+		'label'         => __( 'SLAGELSE', 'slagelselift' ),
+		'section'       => 'footer_options',
+		'settings'      => 'footer_slagelse_vej',
+		'type'          => 'text',
+		'description'   => 'Adresse',
+	) ) );
+	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'footer_slagelse_by', array(
+		'section'       => 'footer_options',
+		'settings'      => 'footer_slagelse_by',
+		'type'          => 'text',
+		'description'   => 'Postnr. & By',
 	) ) );
 
-	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'footer_address', array(
-		'label'    => __( 'Address', 'espenheins' ),
-		'section'  => 'footer_options',
-		'settings' => 'footer_address',
-		'type'     => 'text',
+	// Footer : HERLEV
+	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'footer_herlev_vej', array(
+		'label'         => __( 'HERLEV', 'slagelselift' ),
+		'section'       => 'footer_options',
+		'settings'      => 'footer_herlev_vej',
+		'type'          => 'text',
+		'description'   => 'Addresse',
+	) ) );
+	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'footer_herlev_by', array(
+		'section'       => 'footer_options',
+		'settings'      => 'footer_herlev_by',
+		'type'          => 'text',
+		'description'   => 'Postnr. & By',
 	) ) );
 
-	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'footer_city', array(
-		'label'    => __( 'City', 'espenheins' ),
-		'section'  => 'footer_options',
-		'settings' => 'footer_city',
-		'type'     => 'text',
+	// Footer : CONTACT
+	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'footer_contact_mail', array(
+		'label'         => __( 'KONTAKT', 'slagelselift' ),
+		'section'       => 'footer_options',
+		'settings'      => 'footer_contact_mail',
+		'type'          => 'email',
+		'description'   => 'Mail',
+	) ) );
+	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'footer_contact_tel', array(
+		'section'       => 'footer_options',
+		'settings'      => 'footer_contact_tel',
+		'type'          => 'text',
+		'description'   => 'Telefon',
 	) ) );
 
-	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'footer_cvr', array(
-		'label'    => __( 'CVR', 'espenheins' ),
-		'section'  => 'footer_options',
-		'settings' => 'footer_cvr',
-		'type'     => 'text',
-	) ) );
-
-	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'footer_tel', array(
-		'label'    => __( 'Telephone', 'espenheins' ),
-		'section'  => 'footer_options',
-		'settings' => 'footer_tel',
-		'type'     => 'text',
-	) ) );
-
-	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'footer_email', array(
-		'label'    => __( 'Email', 'espenheins' ),
-		'section'  => 'footer_options',
-		'settings' => 'footer_email',
-		'type'     => 'text',
-	) ) );
-
+	// Footer : IMAGE #1
 	$wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'footer_logo_1', array(
-		'label'    => __( 'Footer logoer 1', 'espenheins' ),
+		'label'    => __( 'FOOTER LOGO #1', 'slagelselift' ),
 		'section'  => 'footer_options',
 		'settings' => 'footer_logo_1',
 	) ) );
 
+	// Footer : IMAGE #2
 	$wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'footer_logo_2', array(
-		'label'    => __( 'Footer logoer 2', 'espenheins' ),
+		'label'    => __( 'FOOTER LOGO #2', 'slagelselift' ),
 		'section'  => 'footer_options',
 		'settings' => 'footer_logo_2',
+	) ) );
+
+	// Footer : IMAGE #3
+	$wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'footer_logo_3', array(
+		'label'    => __( 'FOOTER LOGO #3', 'slagelselift' ),
+		'section'  => 'footer_options',
+		'settings' => 'footer_logo_3',
+	) ) );
+
+	// Footer : IMAGE #4
+	$wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'footer_logo_4', array(
+		'label'    => __( 'FOOTER LOGO #4', 'slagelselift' ),
+		'section'  => 'footer_options',
+		'settings' => 'footer_logo_4',
+	) ) );
+
+	// Footer : DISCLAIMER
+	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'footer_disclaimer', array(
+		'section'       => 'footer_options',
+		'settings'      => 'footer_disclaimer',
+		'type'          => 'text',
+		'description'   => 'DISCLAIMER',
 	) ) );
 }
