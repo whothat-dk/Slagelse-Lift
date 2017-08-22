09a42e0d2572a204df097ea7a3587d0b472079b7
Stefan Korfitz <stefan@korfitz.com>
2017-08-22 16:44:15 +0200

    feature section er designet færdig, og den tager "Nyheder" som indhold... Men der mangler statig click-event på knappen

--- wp-content/themes/whothat/composer/featurePost.php
+++ wp-content/themes/whothat/composer/featurePost.php
@@ -1,8 +1,7 @@
 <?php
 /*
-Element Description: VC employee Box
+Element Description: VC fa icon_Box
 */
-
 
 // Element Class
 class vc_featurePost extends WPBakeryShortCode {
@@ -10,7 +9,7 @@ class vc_featurePost extends WPBakeryShortCode {
 	// Element Init
 	function __construct() {
 		add_action( 'init', array( $this, 'vc_featurePost_mapping' ) );
-		add_shortcode( 'vc_featurePost', array( $this, 'vc_render_featurePost' ) );
+		add_shortcode( 'featurePost', array( $this, 'vc_render_featurePost' ) );
 	}
 
 	// Element Mapping
@@ -31,40 +30,59 @@ class vc_featurePost extends WPBakeryShortCode {
 		// Loop through the records returned
 		$arrPost = array();
 		while ( $findNews->fetch() ) {
-			$arrpost[$findNews->display('news_name')] = $findNews->field('id');
+
+			$arrpost[$findNews->display('title')] = $findNews->field('id');
 		}
+
 		// Map the block with vc_map()
 		vc_map(
 			array(
-				'name' => __('WHOTHAT Featured News', 'featurePost'),
-				'base' => 'vc_featurePost',
+				'name' => __('WHOTHAT Feature Post', 'featurePost'),
+				'base' => 'featurePost',
 				'description' => __('DESC', 'featurePost'),
 				'category' => __('WHOTHAT elements', 'featurePost'),
 				'icon' => get_template_directory_uri().'/assets/icons/ic_vc-element_imagebox.png',
 				'params' => array(
 
+					// Selector (dropdown)
 					array(
-						"type" => "dropdown",
-						'admin_label' => true,
-						"param_name" => "news_single",
-						'group' => 'General',
-						"value" => $arrpost,
-						"description" => __("Random will have higher priority if its checked", "featurePost")
+						"param_name"    => "vc_featurepost_selector",
+						'heading'       => __( 'Select News', 'featurePost' ),
+						"description"   => __("Random will have higher priority if its checked", "featurePost"),
+						'group'         => 'General',
+						'weight'        => 0,
+						"type"          => "dropdown",
+						"value"         => $arrpost
 					),
 
+					// Randomizer (checkbox)
 					array(
-						'type' => 'checkbox',
-						'group' => 'General',
-						'heading' => __( 'Random?', 'featurePost' ),
-						'param_name' => 'randomize',
-						'value' => array( __( '', 'featurePost' ) => true )
+						'param_name'    => 'vc_featurePost_randomizr',
+						'heading'       => __( 'Random?', 'featurePost' ),
+						"description"   => __("Random DESC", "featurePost"),
+						'group'         => 'General',
+						'weight'        => 0,
+						'type'          => 'checkbox',
+						'value'         => array( __( '', 'featurePost' ) => true )
 					),
 
+					// Extra class
 					array(
-						'type' => 'css_editor',
-						'heading' => __( 'Css', 'featurePost' ),
-						'param_name' => 'css',
-						'group' => __( 'Design options', 'featurePost' ),
+						'param_name'    => 'vc_featurePost_extraCSS',
+						'heading'       => __( 'Extra class name', 'featurePost' ),
+						'description'   => __( 'Style particular content element differently - add a class name and refer to it in custom CSS.', 'featurePost' ),
+						'group'         => __( 'Design options', 'featurePost' ),
+						'weight'        => 0,
+						'type'          => 'textfield'
+					),
+					// CSS editor
+					array(
+						'param_name'    => 'vc_featurePost_cssOptions',
+						'heading'       => __( 'Css', 'featurePost' ),
+						'description'   => __( 'xx', 'featurePost' ),
+						'group'         => __( 'Design options', 'featurePost' ),
+						'weight'        => 1,
+						'type'          => 'css_editor'
 					),
 				),
 			)
@@ -72,31 +90,40 @@ class vc_featurePost extends WPBakeryShortCode {
 	}
 
 
+
 	// Element HTML
-	public function vc_render_featurePost ( $atts ) {
+	public function vc_render_featurePost( $atts ) {
 
 		// Params extraction
 		extract(
 			shortcode_atts(
 				array(
-					'news_single' => '',
-					'randomize' => '',
-					'css' => '',
+
+					'vc_featurepost_selector'   => '',
+					'vc_featurePost_randomizr'  => '',
+					'vc_featurePost_extraCSS'   => '',
+					'vc_featurePost_cssOptions' => '',
 				),
 				$atts
 			)
 		);
 
-		if($randomize == 1){
+		if($vc_featurePost_randomizr == 1){
+
 			$params = array(
+
 				'limit' => 1,
 				'orderby' => 'RAND()'
 			);
 
-		} else {
+		}
+
+		else {
+
 			$params = array(
+
 				'limit' => 1,
-				'where' => 't.ID =' . $news_single,
+				'where' => 't.ID =' .$vc_featurepost_selector,
 				'orderby' => 'RAND()'
 			);
 		}
@@ -106,23 +133,28 @@ class vc_featurePost extends WPBakeryShortCode {
 
 		// Loop through the records returned
 		while ( $news->fetch() ) {
-			$img            = get_the_post_thumbnail($news->field('id'));
-			//$heading        = get_the_post_title($news->field('id'));
-			$intro          = $news->display('news_subtext');
-			$content        = $news->display('news_context');
-			$content2       = $news->display('post_content');
-			//$testimonial    = strip_tags($testimonial, "<br>");
+			$img = get_the_post_thumbnail($news->field('id'));
+			$heading = $news->display('title');
+			$subtext = $news->display('subtext');
+			$braking = $news->display('post_content');
 		}
 
+		// ...
+		$css_class = apply_filters( VC_SHORTCODE_CUSTOM_CSS_FILTER_TAG, vc_shortcode_custom_css_class( $vc_featurePost_cssOptions, ' ' ), $this->settings['base'], $atts );
 		// Fill $html var with data
-		$html = '
-        <div class="vc_featurePost_wrap ' . esc_attr( $css ) . '">
-            <div class="vc_featurePost_container">
-                <div  class="vc_featurePost_image">'.$img.'</div>
-                <p class="vc_featurePost_heading">"'.$heading.'"</p>
-                <p class="vc_featurePost_subText">"'.$intro.'"</p>
-            </div>
-        </div>';
+		$html = ' 
+	        <div class="vc_featurePost_wrap ' . $vc_featurePost_extraCSS. '">
+				<div class="featurePost ' .esc_attr( $css_class ). '">
+					<!-- Parsing feat. image into the box  -->
+					<img src='.$img.'
+					<!-- Parsing the label(s) into the box -->
+					<h3>'.$heading.'</h3>
+					<p>'.$subtext.'</p>
+					<!-- Button for reading MORE -->
+					<button id="readMoreBtn" type="submit" value=""/><span><a>Læs hele nyheden</a></span>
+				</div>
+			</div>	
+        ';
 
 		return $html;
 
